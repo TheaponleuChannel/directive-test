6f14c957606fde49249c5c2ece106b785cc29845
TheaponleuChannel <ponleu913@gmail.com>
2023-06-11 21:25:08 +0700

    fix/issue

--- src/app/hightlight.directive.ts
+++ src/app/hightlight.directive.ts
@@ -1,24 +1,26 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHightlight]'
 })
 export class HightlightDirective {
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
   
-  @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('yellow');
-  }
-  
-  // @HostListener('mouseleave') onMouseLeave() {
-  //   this.highlight('green');
-  // }
   @HostListener('a') onMouseLeave(){
 
   }
-  
-  private highlight(color: string) {
-    this.el.nativeElement.style.backgroundColor = color;
+  @HostListener('window:keydown', ['$event'])
+  onKeydown(event: KeyboardEvent) {
+    if (event.key === 'a') {
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'red');
+    }
+    if(event.key === 's' || event.key === 'S'){
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', ' pink');
+    } else if (event.key === 'b') {
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'blue');
+    }
   }
+  
+
 }
